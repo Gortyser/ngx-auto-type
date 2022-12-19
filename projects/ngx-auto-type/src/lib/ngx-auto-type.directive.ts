5c422462b808e7a0fcb28fcbfee74bb4926c7135
Mikhail Filin <mikhailf.codes@gmail.com>
2022-12-19 18:48:19 +0700

    NAT-9: Basic functionality of typing

--- projects/ngx-auto-type/src/lib/ngx-auto-type.directive.ts
+++ projects/ngx-auto-type/src/lib/ngx-auto-type.directive.ts
@@ -1,7 +1,57 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[ngxAutoType]'
 })
-export class NgxAutoTypeDirective {
+export class NgxAutoTypeDirective implements OnInit {
+  private element: HTMLElement | undefined;
+  private originalText: string | undefined;
+  private typingDelay: number = 50;
+
+  public constructor(private elementRef: ElementRef) {
+  }
+
+  public ngOnInit(): void {
+    this.type();
+  }
+
+  public type(): void {
+    this.initialize();
+    this.startTyping();
+  }
+
+  private initialize(): void {
+    this.element = this.elementRef.nativeElement;
+
+    if (!this.element) {
+      return;
+    }
+
+    this.originalText = this.element.innerText;
+    this.element.innerText = '';
+  }
+
+  private startTyping(): void {
+    if (!this.originalText || !this.element) {
+      return;
+    }
+
+    const textLength: number = this.originalText.length;
+    let currentText: string = '';
+    let currentIndex: number = 0;
+
+    const interval: NodeJS.Timer = setInterval(() => {
+      currentText += this.originalText?.[currentIndex];
+
+      if (!this.element) {
+        return;
+      }
+      this.element.innerText = currentText;
+
+      currentIndex++;
+      if (currentIndex === textLength) {
+        clearInterval(interval);
+      }
+    }, this.typingDelay);
+  }
 }
