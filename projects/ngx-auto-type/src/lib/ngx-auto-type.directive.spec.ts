d8418e0557eb5fb4745abedf3816685cdc9f3dee
Mikhail Filin <mikhailf.codes@gmail.com>
2022-12-19 13:58:45 +0400

    NAT-2: Removed unused default component/service (#3)

---
+++ projects/ngx-auto-type/src/lib/ngx-auto-type.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NgxAutoTypeDirective } from './ngx-auto-type.directive';
+
+describe('NgxAutoTypeDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NgxAutoTypeDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- projects/ngx-auto-type/src/public-api.ts
+++ projects/ngx-auto-type/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-auto-type
  */
 
-export * from './lib/ngx-auto-type.service';
-export * from './lib/ngx-auto-type.component';
+export * from './lib/ngx-auto-type.directive';
 export * from './lib/ngx-auto-type.module';
